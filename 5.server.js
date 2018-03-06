5b0e08ffcd894d847ca85b86f8d51164c4b108b9
Hyrum Steffensen <hyrum.steffensen@gmail.com>
2018-03-06 09:38:11 -0800

    created asyncronous chain using asynquence

--- 5.server.js
+++ 5.server.js
@@ -2,8 +2,20 @@ function handleHTTP(req, res) {
     if (req.method === "GET") {
         if (req.url === "/"){
             res.writeHead(200, { "Content-type": "text/plain" });
-            res.write("Hello World: ");
-            res.end("Hello World: " + Math.random());
+
+            ASQ(function(done) {
+                setTimeout(function() {
+                    done(Math.random());
+                }, 1000);
+            })
+            .then(function(done, msg) {
+                setTimeout(function() {
+                    done(msg);
+                }, 2000);
+            })
+            .then(function(_, msg) {
+                res.end("Hello World: " + msg);
+            });
         } else {
             res.writeHead(403);
             res.end("Get outta here!");
@@ -19,3 +31,5 @@ var port = 8006;
 
 var http = require("http");
 var http_serv = http.createServer(handleHTTP).listen(port, host);
+
+var ASQ = require("asynquence");
